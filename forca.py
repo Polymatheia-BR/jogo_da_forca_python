420a22e178234423b074ad78988d1c041f475339
Victor Ferreira <69025753+Polymatheia-BR@users.noreply.github.com>
2023-10-15 21:45:30 -0300

    Adicionado funções

--- forca.py
+++ forca.py
@@ -63,47 +63,63 @@ boneco = [
 '''
 ]
 
+#Checa e substitui caso encontre uma letra
+def checar_letra(vidas):    
+    achou = False    
+    for x in range(len(palavras_dicas[nivel][0])):
+        if jogador_input.upper() == palavras_dicas[nivel][0][x].upper():
+            palavra_atual[x] = palavras_dicas[nivel][0][x]
+            achou = True
 
+    #Se não achar nenhuma letra, subtrai uma vida
+    if achou == False:
+        vidas -= 1
 
-while True:
-    if vidas == 0:
-        print(boneco[vidas])
-        print(f'Você perdeu')
-        break
-    
+    return vidas
+
+#Checa se ainda existem letras a serem achadas
+def avancar_nivel(nivel,palavra_atual):
+    proximo_nivel = True
+    for x in range(len(palavras_dicas[nivel][0])):
+        if palavra_atual[x] == '_':
+            proximo_nivel = False
+
+    return proximo_nivel
+
+def mostrar_jogo():
     print(f'NIVEL: {nivel+1}')
     print(f'DICA: {palavras_dicas[nivel][1]}')
     print(f'VIDAS: {vidas}')
     print(boneco[vidas])
     print(f'PALAVRA: {"".join(palavra_atual)}')
     print(f'LETRAS DIGITADAS: {" ".join(letras_digitadas).upper()}')
-    letra = input('Digite uma letra: ')
-    letras_digitadas = letras_digitadas + [letra]
+    jogador_input = input('Digite uma letra: ')
+    while len(jogador_input) > 1:
+        jogador_input = input('Valor invalido! Digite apenas uma letra: ')
     print('----------')
+
+    return jogador_input
     
-    #Checa e substitui caso encontre uma letra    
-    achou = False    
-    for x in range(len(palavras_dicas[nivel][0])):
-        if letra.upper() == palavras_dicas[nivel][0][x].upper():
-            palavra_atual[x] = palavras_dicas[nivel][0][x]
-            achou = True
-            
-    if achou == False:
-        vidas -= 1
+#Loop principal do jogo
+while True:
+    if vidas == 0:
+        print(boneco[vidas])
+        print(f'Você perdeu')
+        break
+    
+    jogador_input = mostrar_jogo()
+    letras_digitadas = letras_digitadas + [jogador_input]
+    vidas = checar_letra(vidas)
 
-    #Checa se ainda existem letras a serem achadas
-    proximo_nivel = True
-    for x in range(len(palavras_dicas[nivel][0])):
-        if palavra_atual[x] == '_':
-            proximo_nivel = False
-
-    if proximo_nivel == True: #Avança para o proximo nível se não encontrar undeline
+    #Avança para o proximo nivel
+    if avancar_nivel(nivel, palavra_atual): 
         nivel += 1
         if nivel == 5:
             print(f'Você ganhou o jogo! Receba aqui seu premio 🏆')
             break
         palavra_atual = list(len(palavras_dicas[nivel][0]) * '_')
         vidas = 6
+        letras_digitadas = []
         print(f'==========\nPROXIMO NÍVEL\n==========')
 
 print(f'FIM DE JOGO')
